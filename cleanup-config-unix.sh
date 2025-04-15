addf7ee86ff4c618d10d997d451fcbd5b1c8b7a5
Yilin Fang <qzfyl98@outlook.com>
2025-04-15 14:48:30 -0400

    Update cleanup-config-unix.sh

--- cleanup-config-unix.sh
+++ cleanup-config-unix.sh
@@ -19,28 +19,12 @@ clean_tmux() {
   echo "Cleaning up tmux configuration...done"
 }
 
-# Function to clean Zellij configuration
-clean_zellij() {
-  echo "Cleaning up Zellij configuration..."
-  rm -rf ~/.config/zellij
-  echo "Cleaning up Zellij configuration...done"
-}
-
-# Function to clean Yazi configuration
-clean_yazi() {
-  echo "Cleaning up Yazi configuration..."
-  rm -rf ~/.config/yazi
-  echo "Cleaning up Yazi configuration...done"
-}
-
 # Menu to select which tools to clean
 echo "Select the tools to clean (you can select multiple, e.g., '1 2'):"
 echo "1. Neovim"
 echo "2. Tmux"
-echo "3. Zellij"
-echo "4. Yazi"
-echo "5. All"
-read -p "Enter your choice(s) [1-5]: " choices
+echo "a. All"
+read -p "Enter your choice(s): " choices
 
 for choice in $choices; do
   case $choice in
@@ -50,20 +34,12 @@ for choice in $choices; do
   2)
     clean_tmux
     ;;
-  3)
-    clean_zellij
-    ;;
-  4)
-    clean_yazi
-    ;;
-  5)
+  a)
     clean_nvim
     clean_tmux
-    clean_zellij
-    clean_yazi
     ;;
   *)
-    echo "Invalid choice: $choice. Please enter a number between 1 and 5."
+    echo "Invalid choice: $choice."
     ;;
   esac
 done
