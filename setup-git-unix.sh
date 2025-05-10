bd139feb030d7167d357188c5ee565a7057ae200
Yilin Fang <qzfyl98@outlook.com>
2025-05-10 09:01:12 -0400

    Add setup-git-unix.sh

---
+++ setup-git-unix.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Add [alias] section
+git config --global alias.a "add"
+git config --global alias.c "checkout"
+git config --global alias.m "merge"
+git config --global alias.aa "add -A"
+git config --global alias.cb "checkout -b"
+git config --global alias.ci "commit"
+git config --global alias.cm "commit -m"
+git config --global alias.st "status"
+git config --global alias.pl "pull"
+git config --global alias.ph "push"
+git config --global alias.dl "-c diff.external=difft log -p --ext-diff"
+git config --global alias.ds "-c diff.external=difft show --ext-diff"
+git config --global alias.dft "-c diff.external=difft diff"
+
+# Add [core] section
+git config --global core.pager "delta"
+
+# Add [interactive] section
+git config --global interactive.diffFilter "delta --color-only"
+
+# Add [delta] section
+git config --global delta.navigate "true"
+
+# Add [merge] section
+git config --global merge.conflictStyle "zdiff3"
+
+echo "Git configuration set up successfully."
